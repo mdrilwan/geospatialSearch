e2b2357ec5c3fa491581d13416d4751be3e7105d
Mohamed Rilwan J <mohamed_rilwan@thbs.india.com>
2019-02-24 21:15:40 +0530

    Initial Version

---
+++ WebContent/js/geosearch.js
@@ -0,0 +1,109 @@
+var cluster1;
+var cluster2;
+var map;
+
+$(document)
+		.ready(
+				function() {
+
+					cluster1 = new L.MarkerClusterGroup({
+						animateAddingMarkers : true
+					});
+					cluster2 = new L.MarkerClusterGroup({
+						animateAddingMarkers : true
+					});
+
+					var southWest = new L.latLng(-60.11605, -228.51562), northEast = L
+							.latLng(80.01954, 228.86719), bounds = L
+							.latLngBounds(southWest, northEast);
+
+					var layer = L
+							.tileLayer(
+									'http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}',
+									{
+										attribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'
+									});
+
+					map = L.map('mapid', {
+						minZoom : 2,
+						maxZoom : 10,
+						layers : [ layer ]
+					});
+
+					map.fitBounds(bounds);
+					// map.fitWorld();
+					map.setMaxBounds(map.getBounds());
+
+					getAllData();
+
+				});
+
+function getAllData() {
+	$('#lat').val("");
+	$('#long').val("");
+	map.removeLayer(cluster2);
+	cluster1 = new L.MarkerClusterGroup({
+		animateAddingMarkers : true
+	});
+	$.ajax({
+		type : 'GET',
+		// map.fitWorld();
+		url : '/geospatialSearch/rest/geoSearch/getData',
+		dataType : 'JSON',
+		success : function(response) {
+
+			if (response.length > 0) {
+
+				for (var i = 0; i < response.length; i++) {
+
+					cluster1.addLayer(new L.Marker(
+							response[i].location.coordinates)
+							.bindPopup(response[i].name + "\t"
+									+ response[i].location.coordinates));
+				}
+			}
+			map.addLayer(cluster1);
+		},
+
+		error : function(xhr, status, txt) {
+			alert("unsuccess");
+		}
+	});
+}
+
+function geoSpatial() {
+	map.removeLayer(cluster1);
+	cluster2 = new L.MarkerClusterGroup({
+		animateAddingMarkers : true
+	});
+
+	var url = '/geospatialSearch/rest/geoSearch/getLocationData';
+	url = url + "/" + document.getElementById("lat").value + "/"
+			+ document.getElementById("long").value;
+	lati = document.getElementById("lat").value;
+	longi = document.getElementById("long").value;
+
+	$.ajax({
+		type : 'GET',
+		url : url,
+		dataType : 'JSON',
+		success : function(response) {
+
+			if (response.length > 0) {
+
+				for (var i = 0; i < response.length; i++) {
+
+					cluster2.addLayer(new L.Marker(
+							response[i].location.coordinates)
+							.bindPopup(response[i].name + "\t"
+									+ response[i].location.coordinates));
+				}
+			}
+			map.addLayer(cluster2);
+		},
+
+		error : function(xhr, status, txt) {
+			alert("unsuccess");
+		}
+	});
+}
---
+++ src/com/thbs/geoSpatial/MongoRest.java
@@ -0,0 +1,77 @@
+package com.thbs.geoSpatial;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+import org.bson.types.BasicBSONList;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+
+@Path("/geoSearch")
+public class MongoRest {
+
+	MongoClient client = new MongoClient();
+	MongoCollection<Document> collection = client.getDatabase("GeosaptialData").getCollection("restaurants");
+
+	@GET
+	@Path("/getData")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getData() throws IOException, ParseException {
+
+		JSONArray result = new JSONArray();
+
+		MongoCursor<Document> cursor = collection.find().iterator();
+
+		while (cursor.hasNext()) {
+			Document doc = cursor.next();
+			doc.remove("_id");
+			result.add(doc);
+		}
+		return Response.ok().entity(result.toString()).build();
+	}
+
+	@GET
+	@Path("/getLocationData/{lat}/{long}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getLocationData(@PathParam("lat") String lati, @PathParam("long") String longi)
+			throws IOException, ParseException {
+
+		JSONArray result = new JSONArray();
+
+		Document location = new Document();
+		BasicBSONList centerSphereQuery = new BasicBSONList();
+		BasicBSONList points = new BasicBSONList();
+		points.add(Double.parseDouble(lati));
+		points.add(Double.parseDouble(longi));
+		centerSphereQuery.add(points);
+		centerSphereQuery.add(5 / 3963.2);
+
+		location.put("$geoWithin", new Document("$centerSphere", centerSphereQuery));
+
+		Document query = new Document();
+		query.put("location", location);
+
+		MongoCursor<Document> cursor = collection.find(query).iterator();
+
+		while (cursor.hasNext()) {
+			Document doc = cursor.next();
+			doc.remove("_id");
+			result.add(doc);
+		}
+		// System.out.println(result);
+
+		// System.out.println(rows);
+		return Response.ok().entity(result.toString()).build();
+	}
+}
